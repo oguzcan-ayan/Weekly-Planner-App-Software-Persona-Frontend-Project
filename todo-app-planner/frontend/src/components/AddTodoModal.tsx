5d777e3b8d8a093676e8c85f092b0eec513259c3
Oğuzcan Ayan <oguzcanayan43@hotmail.com>
2026-02-04 23:33:25 +0300

    add-some-days

--- todo-app-planner/frontend/src/components/AddTodoModal.tsx
+++ todo-app-planner/frontend/src/components/AddTodoModal.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { type Todo } from "../interfaces/todo"
 
-const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 interface Props {
   defaultDay: string
